6f8cf4ef66ac154baea765ea9932e284540ecdd0
asiviero <altsiviero@gmail.com>
2013-07-23 10:25:23 +0000

    Fetch geolocation

---
+++ map.js
@@ -0,0 +1,89 @@
+var map;
+function initialize() {
+	
+        var mapOptions = {
+          center: new google.maps.LatLng(geoip_latitude(),geoip_longitude()),
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("google-map"),
+            mapOptions);
+        console.log(new google.maps.LatLng(geoip_latitude(),geoip_longitude()));
+
+        //console.log("Pasou");
+        //console.log(navigator);
+        if(navigator.geolocation) {
+    	    browserSupportFlag = true;
+    	    console.log("Aqui");
+    	    navigator.geolocation.getCurrentPosition(function(position) {    	    	
+    	      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    	      console.log(initialLocation);
+    	      map.setCenter(initialLocation);
+    	      var marker = new google.maps.Marker({
+    	            position: initialLocation,
+    	            map: map,
+    	            title: 'Hello World!'
+    	        });
+    	        
+    	    }, function() {
+    	    	console.log("fial");
+    	      handleNoGeolocation(browserSupportFlag);
+    	    }, {maximumAge:60000, timeout:5000, enableHighAccuracy:true});
+    	  }
+        else {
+        	console.log("Aq3");
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+          }
+
+      }
+google.maps.event.addDomListener(window, 'load', initialize);
+
+var socket = io.connect('http://ec2-54-213-75-113.us-west-2.compute.amazonaws.com:8080');
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
+socket.on('message', function(data) {
+	window.alert('Ola Denis!');
+})
+
+/*if (google.loader.ClientLocation != null) {
+  alert(google.loader.ClientLocation.address.city);
+} else {
+  alert("Not found");
+}*/
+console.log(new google.maps.LatLng(geoip_latitude(),geoip_longitude()));
+
+function showLocation(position) {
+	  var latitude = position.coords.latitude;
+	  var longitude = position.coords.longitude;
+	  //alert("Latitude : " + latitude + " Longitude: " + longitude);
+	  map.setCenter(new google.maps.LatLng(latitude,longitude));
+	  console.log(map);
+	  var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latitude,longitude),
+          map: map,
+          title: 'Hello World!'
+      });
+	}
+
+	function errorHandler(err) {
+	  if(err.code == 1) {
+	    alert("Error: Access is denied!");
+	  }else if( err.code == 2) {
+	    alert("Error: Position is unavailable!");
+	  }
+	}
+	function getLocation(){
+
+	   if(navigator.geolocation){
+	      // timeout at 60000 milliseconds (60 seconds)
+	      var options = {timeout:60000};
+	      navigator.geolocation.getCurrentPosition(showLocation, 
+	                                               errorHandler,
+	                                               options);
+	   }else{
+	      alert("Sorry, browser does not support geolocation!");
+	   }
+	}
--- web.js
+++ web.js
@@ -1,15 +1,131 @@
 var express = require('express');
 var fs = require('fs');
+var http = require('http');
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname));
+//var io = require('socket.io');
 
+//Start the server at port 8080
+/*var server = http.createServer(function(req, res){ 
+	//this.use(express.static(__dirname));
+    // Send HTML headers and message
+    //res.writeHead(200,{ 'Content-Type': 'text/html' }); 
+    //res.end('<h1>Hello Socket Lover!</h1>');
+	console.log(req.url);
+	if(req.url == '/') {
+		buf = fs.readFileSync('index.html');
+		res.end(buf.toString());
+	}
+	else if(req.url=='/map') {
+		buf = fs.readFileSync('map.html');
+		res.end(buf.toString());
+	}
+	else if(req.url=='/map.js') {
+		buf = fs.readFileSync('map.js');
+		res.end(buf.toString());
+	}
+});
+server.listen(8080);
+// Create a Socket.IO instance, passing it our server
+var socket = io.listen(server);
+
+// Add a connect listener
+socket.on('connection', function(client){ 
+    console.log("server is start on port 8080");
+
+    // Create periodical which ends a message to the client every 5 seconds
+    var interval = setInterval(function() {
+        client.send('This is a message from the server!  ' + new Date().getTime());
+    },5000);
+
+    // Success!  Now listen to messages to be received
+    client.on('message',function(event){
+        console.log('Received message from client! ',event);
+    });
+    client.on('disconnect',function(){
+        clearInterval(interval);
+        console.log('Server has disconnected');
+    });
+
+});*/
+/*
 app.get('/', function(request, response) {
 	var content;
 	buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
+app.get('/map', function(request, response) {
+	var content;
+	buf = fs.readFileSync('map.html');
+	//require('map.js');
+  response.send(buf.toString());
+	//response.render('map.html');
+});
 
 var port = process.env.PORT || 8080 ;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});*/
+
+var app = require('http').createServer(handler)
+, io = require('socket.io').listen(app)
+, fs = require('fs')
+
+app.listen(8080);
+
+function handler (req, res) {
+	if(req.url == "/") {
+		url = "/index.html";
+	}
+	else {
+		url = req.url;
+	}
+	fs.readFile(__dirname + url,
+			function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	/*socket.on('my other event', function (data) {
+		console.log(data);
+	});*/
+	socket.on('message',function(event){
+        console.log('Received message from client! ',event);
+        if(event == 'Ola Zubat!') {
+        	socket.emit('message','Ola!');
+        }
+	});
+	
 });
+
+//Create a Socket.IO instance, passing it our server
+/*var socket = io.sockets.listen(app);
+
+// Add a connect listener
+socket.on('connection', function(client){ 
+    console.log("server is start on port 8080");
+
+    // Create periodical which ends a message to the client every 5 seconds
+    var interval = setInterval(function() {
+        client.send('This is a message from the server!  ' + new Date().getTime());
+    },5000);
+
+    // Success!  Now listen to messages to be received
+    client.on('message',function(event){
+        console.log('Received message from client! ',event);
+    });
+    client.on('disconnect',function(){
+        clearInterval(interval);
+        console.log('Server has disconnected');
+    });
+
+})*/;
